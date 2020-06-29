e6ac4bf228dea7fd28a23644979faf27ddc1f745
M. Mücahit Tezcan <mmucahit.tezcan@gmail.com>
2020-06-29 17:37:56 +0300

    Remix icon removed from cnd and installed npm way 🐱‍🏍

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
-import Vue from "vue";
-import App from "./App.vue";
-import vueSmoothScroll from "vue2-smooth-scroll";
-Vue.use(vueSmoothScroll);
+import Vue from 'vue'
+import App from './App.vue'
+import vueSmoothScroll from 'vue2-smooth-scroll'
+Vue.use(vueSmoothScroll)
+import 'remixicon/fonts/remixicon.css'
 
-import VueMeta from "vue-meta";
+import VueMeta from 'vue-meta'
 
-Vue.use(VueMeta);
+Vue.use(VueMeta)
 // animation
-import ScrollAnimation from "./components/animation/scrollAnimation";
+import ScrollAnimation from './components/animation/scrollAnimation'
 
-Vue.directive("scrollanimation", ScrollAnimation);
+Vue.directive('scrollanimation', ScrollAnimation)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered"))
-}).$mount("#app");
+  render: (h) => h(App),
+  mounted: () => document.dispatchEvent(new Event('x-app-rendered')),
+}).$mount('#app')
